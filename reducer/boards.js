0127f13c1354aa20aba7e0889a46cd222e3c2d09
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-29 12:48:10 -0700

    CATEGORY_UPDATE

--- reducer/boards.js
+++ reducer/boards.js
@@ -5,6 +5,12 @@ let initialState = []
 let reducer = (state=initialState, action) {
   let {type, payload} = action;
   switch(type) {
+    case 'CATEGORY_CREATE':
+      return [...state, payload];
+    case 'CATEGORY_UPDATE':
+      return state.map(category =>
+        category.id == payload.id ? payload : category)
+    case 'CATEGORY_DELETE':
     default:
       return state;
   }

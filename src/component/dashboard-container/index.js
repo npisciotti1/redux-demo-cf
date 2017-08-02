65c07c5b6334b02c858211309b9041c4b269f7eb
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-02 14:29:16 -0700

    DashboardContainer.js

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -5,3 +5,35 @@ import {categoryCreate, categoryUpdate, categoryDelete} from '../../action/categ
 
 import CategoryForm from '../category-form';
 import CategoryItem from '../category-render';
+
+class DashboardContainer extends React.Component {
+
+  render() {
+    return (
+      <main className='dashboard-container'>
+        <h2> dashboard </h2>
+        <CategoryForm
+          buttonText='create category'
+          onComplete={this.props.categoryCreate}
+        />
+
+        <CategoryItem
+          categoryUpdate={this.props.categoryUpdate}
+          categoryDelete={this.props.categoryDelete}
+        />
+      </main>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({categories: state})
+
+const mapDispatchToProps = (dispatch, getState) => (
+  {
+    categoryCreate: (category) => dispatch(categoryCreate(category)),
+    categoryUpdate: (category) => dispatch(categoryUpdate(category)),
+    categoryDelete: (category) => dispatch(categoryDelete(category))
+  }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer)

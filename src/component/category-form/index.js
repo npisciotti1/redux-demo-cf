9ef268c0660455c0e926a1d6c516dac86a781599
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-02 14:46:31 -0700

    added category-form component

--- src/component/category-form/index.js
+++ src/component/category-form/index.js
@@ -22,4 +22,35 @@ class CategoryForm extends React.Component {
     console.log('I\'m rendering:', this.props.category);
     this.props.onComplete(Object.assign({}, this.state));
   }
+
+  render() {
+    return(
+      <form className='category-form' onSubmit={this.handleSubmit}>
+        <input
+          className='title-input'
+          name='name'
+          type='text'
+          placeholder='title'
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+
+        <input
+          className='price-input'
+          name='budget'
+          type='number'
+          value={this.state.price}
+          onChange={this.handleChange}
+        />
+
+        <button
+          className='button-input'
+          type='submit'>
+          {this.props.buttonName}
+        </button>
+      </form>
+    )
+  }
 }
+
+export defaut CategoryForm;

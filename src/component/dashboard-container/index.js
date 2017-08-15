d1ffb651134f524b28c4bbdc92b375b1dd91f2ad
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-15 11:49:05 -0700

    simple component to render

--- src/component/dashboard-container/index.js
+++ src/component/dashboard-container/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
+
+
+class DashboardContainer extends React.Component {
+  render(){
+    return(
+      <main className="dashboard-container">
+        <h2> dashboard </h2>
+      </main>
+    )
+  }
+}
+
+export default DashboardContainer;

30f3bba312d4ae5e00f60902f329ffdcf0645ff0
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-16 18:39:27 -0700

    back to previous build

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './component/app';
-//
-// ReactDOM.render(<App/>, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/app';
 
-import {createStore} from 'redux';
-import categoryReducer from './reducer/category.js';
-import {
-  categoryCreate,
-  categoryUpdate,
-  categoryDelete,
-  categoryReset
-} from './action/category-actions';
-
-const store = createStore(categoryReducer);
-
-console.log('store:', store);
-
-store.dispatch(categoryCreate({title: 'coolbeans'}))
-
-console.log('state', store.getState())
+ReactDOM.render(<App/>, document.getElementById('root'));

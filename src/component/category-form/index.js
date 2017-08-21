915a7f33c29d69aa598719c5be3566de980cafc2
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-20 22:28:59 -0700

    handleChange() and handleSubmit()

--- src/component/category-form/index.js
+++ src/component/category-form/index.js
@@ -3,6 +3,20 @@ import 'React' from 'react';
 class CategoryForm extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(e) {
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {}
 }

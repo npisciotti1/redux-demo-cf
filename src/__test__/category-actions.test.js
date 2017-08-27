0d4c0ece7f36a49f844c552bde996fee80084d59
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-08-26 21:40:56 -0700

    added tests for categoryUpdate and categoryDelete

--- src/__test__/category-actions.test.js
+++ src/__test__/category-actions.test.js
@@ -13,4 +13,18 @@ describe('testing category actions', () => {
     expect(action.payload.timestamp).toBeTruthy()
     expect(action.payload.title).toEqual('test');
   })
+
+  test('categoryUpdate returns a CATEGORY_UPDATE action', () => {
+    let action = categoryUpdate({title: 'update test'})
+
+    expect(action.type).toEqual('CATEGORY_UPDATE');
+    expect(action.payload.title).toEqual('update test');
+  })
+
+  test('categoryDelete returns a CATEGORY_DELETE action', () => {
+    let action = categoryDelete({title: 'delete test'});
+
+    expect(action.type).toEqual('CATEGORY_DELETE');
+    expect(action.payload.title).toEqual('delete test');
+  })
 })

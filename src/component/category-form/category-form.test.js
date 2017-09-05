08df5cb277b93d52ee1c9c37fc84b95ba94b9f02
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-09-04 19:42:39 -0700

    desc for enzyme and jest methods in test file

--- src/component/category-form/category-form.test.js
+++ src/component/category-form/category-form.test.js
@@ -4,15 +4,19 @@ import CategoryForm from './index.js'
 
 describe('testing CategoryForm', () => {
   test('onComplete() should be called with the state on submit', () => {
+    //jest has a native 'mock function' that allows us to keep track of when
+    //it's called, useful for our onComplete()
     let mockHandleOnComplete = jest.fn();
 
+    //we create a 'wrapper' component with mount() from enzyme. By creating this
+    //mock component, we can pass in our mock function from jest.
     let wrapper = mount(
       <CategoryForm
         onComplete={mockHandleOnComplete}
         buttonText='cool'
         />
       )
-
+      
     let mockState = {title: 'test'};
     wrapper.setState(mockState);
 

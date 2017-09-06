8b0b2b372108e322dd94bf7c219337d0b1a73d50
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-09-06 15:57:55 -0700

    added redux-reporter.js middleware

---
+++ src/lib/redux-reporter.js
@@ -0,0 +1,16 @@
+let reporter = store => next => action => {
+  console.log('__ACTION__', action);
+
+  try {
+    let result = next(action);
+    console.log('__STATE__', store.getState());
+    return result;
+
+  } catch(error) {
+    error.action = error;
+    console.error('__ERROR__', error);
+    return error;
+  }
+}
+
+export default reporter;

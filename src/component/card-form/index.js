7f0e5cf33b61a7affb0c4d8738a954bef36a78c7
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-09-07 12:50:17 -0700

    handleChange() and componenetWillRecieveProps()

--- src/component/card-form/index.js
+++ src/component/card-form/index.js
@@ -3,18 +3,20 @@ import React from 'react';
 class CardForm extends React.Component {
   constructor(props){
     super(props)
+    this.state = props.card ? {...props.card} : {title: '', categoryID: props.categoryID}
   }
 
   componentWillReceiveProps(props) {
-
+    if(props.card) this.setState({...props.card})
   }
 
   handleChange(e){
-
+    this.setState({title: e.target.value})
   }
 
   handleSubmit(e){
-
+    e.preventDefault();
+    
   }
 
   render(){

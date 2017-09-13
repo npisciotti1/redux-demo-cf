4198d5f5f3086087aa16118c0ee16bc0248ee53f
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-13 16:51:46 -0700

    small typo fix

--- src/component/card-item/index.js
+++ src/component/card-item/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-category CardItem extends React.Component{
+class CardItem extends React.Component{
   render() {
     let {card} = this.props;
     return(

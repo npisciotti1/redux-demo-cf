1de9f30ee19cde0e58c18dd577dae8fe30cf2bca
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-13 15:40:09 -0700

    added CardItem component

---
+++ src/component/card-item/index.js
@@ -0,0 +1,15 @@
+import 'React' from 'react';
+import {connect} from 'react-redux';
+
+category CardItem extends React.Component{
+  render() {
+    let {card} = this.props;
+    return(
+      <li className='card-item'>
+        <p> {card.content} </p>
+      </li>
+    )
+  }
+}
+
+export default CardItem;

058b29cc9339a4375b89c7c03dccffb612a357e6
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-21 13:50:01 -0700

    first test passing for DashboardContainer

--- src/component/dashboard-container/dashboard-container.test.js
+++ src/component/dashboard-container/dashboard-container.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import createAppStore from '../../lib/store.js';
 import DashboardContainer from './index.js';
+
+describe('DashboardContainer test suite', () => {
+  test('it should have a category props from the store', () => {
+
+    let mockStore = createAppStore();
+    let wrapper = shallow(<DashboardContainer store={mockStore}/>)
+
+    expect(wrapper.props().categories).toEqual([])
+  })
+})

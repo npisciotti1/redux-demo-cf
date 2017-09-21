87b17fff159417352ae3f0508f00115c3789fe13
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-21 14:41:30 -0700

    second DashboardContainer test passing

--- src/component/dashboard-container/dashboard-container.test.js
+++ src/component/dashboard-container/dashboard-container.test.js
@@ -12,4 +12,18 @@ describe('DashboardContainer test suite', () => {
 
     expect(wrapper.props().categories).toEqual([])
   })
+
+  test('it should be able to create categories', () => {
+
+    let mockStore = createAppStore();
+    let wrapper = shallow(<DashboardContainer store={mockStore}/>)
+
+    //categories array should be empty before creating a category
+    expect(wrapper.props().categories).toEqual([]);
+
+    wrapper.props().categoryCreate({title: 'cool'});
+
+    let wrapperState = wrapper.props().store.getState()
+    expect(wrapperState.categories[0].title).toEqual('cool')
+  })
 })

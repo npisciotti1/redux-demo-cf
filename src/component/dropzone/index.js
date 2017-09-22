4fcb6f252b86bf74fa26d95f445e049da2825c9c
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-22 14:52:56 -0700

    adding handleDragOver() to Dropzone component

--- src/component/dropzone/index.js
+++ src/component/dropzone/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 class Dropzone extends React.Component {
+  constructor(props){
+    super(props)
 
+    this.handleDragOver = this.handleDragOver.bind(this);
+    this.handleDrop = this.handleDrop.bind(this);
+  }
+
+  handleDragOver(e){
+    e.preventDefault()
+  }
+
+  render() {
+    (
+      <div
+        className='drop-zone'
+        onDragOver={this.handleDragOver}
+        onDrop={this.handleDrop}
+        >
+        {this.props.children}
+      </div>
+    )
+  }
 }
 
 export default Dropzone;

b24db1ce1de7724d416122acd493e205d49edc44
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-22 14:34:27 -0700

    adding handleDragStart() handler to draggable component

--- src/component/draggable/index.js
+++ src/component/draggable/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+
+//props:
+//  dataTransferItem -- will be stored as a JSON object on the 'drag' event.
+//
 class Draggable extends React.Component {
+  constructor(){
+    this.handleDragStart = this.handleDragStart.bind(this);
+  }
+
+  handleDragStart(e) {
+    e.preventDefault();
+  }
   render(){
     return (
-      <div className='draggable' draggable='true'>
+      <div
+        className='draggable'
+        draggable='true'
+        onDragStart={this.handleDragStart}
+        >
         {this.props.children}
       </div>
     )

3fbf7d4642f322eb94e0157645548252eed4ae63
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-25 15:38:18 -0700

    classToggler()

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,3 +2,11 @@
 
 //Checks if truthy, useful for hiding/components.
 export const renderIf = (test, component) => test ? component : undefined;
+
+//accepts a config object, in this case an object that corresponds to class names for
+//elements, if the classname has a truthy value, it gets returned. Example config:
+// { 'showChild': true, 'isHidden': false }
+//This would only return 'showChild'.
+export const classTogler = (config) => {
+  Object.keys(config).filter(key => config[key]).join(' ')
+}

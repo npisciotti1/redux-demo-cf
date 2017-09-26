16b9f327af814f9c08d15d5c2aa7551b3347d90e
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-26 13:57:23 -0700

    changed logs in redux-reporter.js middleware

--- src/lib/redux-reporter.js
+++ src/lib/redux-reporter.js
@@ -1,14 +1,16 @@
+import {log, logError} from './util.js'
+
 let reporter = store => next => action => {
-  console.log('__ACTION__', action);
+  log('__ACTION__', action);
 
   try {
     let result = next(action);
-    console.log('__STATE__', store.getState());
+    log('__STATE__', store.getState());
     return result;
 
   } catch(error) {
     error.action = action;
-    console.error('__ERROR__', error);
+    logError('__ERROR__', error);
     return error;
   }
 }

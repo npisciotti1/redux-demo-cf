e4e5c812375b0de97e746dbafd8ace2ce2591bb3
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-26 16:34:25 -0700

    adding test suite for util file

---
+++ src/__test__/util.js
@@ -0,0 +1,23 @@
+import {classToggler, renderIf, log, logError} from '../lib/util.js'
+
+describe('testing util helpers', () => {
+  describe('testing loggers', () => {
+    let debugCache;
+    beforeAll(() => {
+      if(global.__DEBUG__) debugCache = global.__DEBUG__;
+    })
+
+    afterAll(() => {
+      global.__DEBUG__ = debugCache;
+    })
+
+    test('log should work when __DEBUG__ is true', () => {
+      global.__DEBUG__ = true;
+
+      //jest's spy method to keep track of whether console.log is called
+      const spy = jest.spyOn(console, 'log');
+
+      
+    })
+  })
+})

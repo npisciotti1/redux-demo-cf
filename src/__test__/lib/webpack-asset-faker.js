d87cb8602ddd4136b15be165144a4088b76851f5
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-29 11:40:23 -0700

    added webpack-asset-faker to ignore .scss files when running jest

---
+++ src/__test__/lib/webpack-asset-faker.js
@@ -0,0 +1,4 @@
+//This file is a 'replacement' or 'dummy' file that we have jest load
+//when it encounters any file with .scss
+
+export default () => {}
--- src/component/draggable/draggable.test.js
+++ src/component/draggable/draggable.test.js
@@ -6,5 +6,7 @@ describe('testing draggable component', () => {
   test('should store JSON on e.dataTransfer', () => {
     let item = {example: 'data'}
     let wrapper = mount(<Draggable dataTransferItem={item} />)
+
+    wrapper.find('.draggable').simulate('dragstart')
   })
 })

93ddc22fa2c6695858ed155fecaf02026d5979f8
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-29 11:42:13 -0700

    desc from Draggable component test file

--- src/component/draggable/draggable.test.js
+++ src/component/draggable/draggable.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {mount} from 'enzyme';
 import Draggable from './index.js';
 
+//Currently can't test the DnD API since Enzyme uses JSDOM which doesn't provide
+//any direct way of testing.
 describe('testing draggable component', () => {
   test('should store JSON on e.dataTransfer', () => {
     let item = {example: 'data'}

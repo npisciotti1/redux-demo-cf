307dd6ad2dd0649f4d3b08589d5093ed40b9feef
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-29 11:25:54 -0700

    adding draggable test suite

---
+++ src/component/draggable/draggable.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Draggable from './index.js';
+
+describe('testing draggable component', () => {
+  test('should store JSON on e.dataTransfer', () => {
+    let item = {example: 'data'}
+    let wrapper = mount(<Draggable dataTransferItem={item} />)
+  })
+})
